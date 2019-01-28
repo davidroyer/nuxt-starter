56bef9e46ada1872a5151466ff0aa2ed7d9f2ae9
David Royer <droyer01@gmail.com>
2019-01-28 18:07:45 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:prettier/recommended'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+      "vue/html-self-closing": ["error", {
+        "html": {
+          "void": "always",
+          "normal": "never",
+          "component": "always"
+        }
+      }]
+    }    
+}
